4d3d16584579453d1544d3adde829da117d4a75c
Gabriela Parra <parragabriela@icloud.com>
2017-07-17 09:53:42 -0400

    added a second counter component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MyCounterComponent } from './my-counter/my-counter.component';
+import { MyCounter2Component } from './my-counter-2/my-counter-2.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyCounterComponent
+    MyCounterComponent,
+    MyCounter2Component
   ],
   imports: [
     BrowserModule,
---
+++ src/app/my-counter-2/my-counter-2.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { CounterService } from '../counter.service';
+
+
+@Component({
+  selector: 'app-my-counter-2',
+  templateUrl: 'my-counter-2.component.html',
+  styles: [],
+  providers: [CounterService]
+})
+
+export class MyCounter2Component implements OnInit {
+  constructor(private theCounter: CounterService) {}
+
+  ngOnInit() {}
+
+  incrementCounter() {
+    this.theCounter.increment();
+  }
+}
